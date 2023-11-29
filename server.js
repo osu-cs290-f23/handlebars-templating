fae8db851de0f2ffc508be8718d10a7bd23c99bc
Rob Hess <hessro@oregonstate.edu>
2023-11-29 15:49:53 -0800

    Add code from 11/29 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express')
+var exphbs = require("express-handlebars")
 
 var port = process.env.PORT || 8000
 var app = express()
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+var peopleData = require("./peopleData.json")
+console.log("== peopleData:", peopleData)
 
 app.use(express.static('static'))
 
@@ -9,27 +16,22 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/static/people.html')
 })
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-]
-
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase()
-  if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/static/people/' + person + '.html'
-    )
+  var personData = peopleData[person]
+  if (personData) {
+    res.status(200).render("photoPage", {
+      name: personData.name,
+      photos: personData.photos,
+      displayTheParagraph: true
+    })
   } else {
     next()
   }
 })
 
 app.get("*", function (req, res, next) {
-  res.status(404).sendFile(__dirname + '/static/404.html')
+  res.status(404).render("404", { url: req.originalUrl })
 })
 
 app.listen(port, function (err) {
